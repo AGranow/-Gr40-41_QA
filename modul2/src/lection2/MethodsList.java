b920d21bbef8454cbe962fda38f31b4dd036c52d
Avraham Granov <Avraham.granow@gmail.com>
2023-01-20 13:05:50 +0100

    add lecture2 modul2

---
+++ modul2/src/lection2/MethodsList.java
@@ -0,0 +1,30 @@
+package lection2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodsList {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList();
+// TODO Прокомментировать код
+        list.add(5); // 5
+        list.add(7);  // 5,7
+        list.add(9);  //5/7/9
+        list.add(0);  // 5,7,9,0
+
+        list.add(1,3);// 5,3,7,9,0
+        list.get(2);
+        list.remove(2);
+        list.clear();
+        list.indexOf(9);
+        list.isEmpty();
+        list.contains(3);
+        list.removeAll(list);
+    }
+    class Person{
+
+    }
+
+}
